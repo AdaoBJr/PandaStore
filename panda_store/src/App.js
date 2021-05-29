0a8431386030886eebb4b438a7cde46307dea53c
Adão Benites <adaobjr@gmail.com>
2021-05-29 10:51:51 -0400

    feat: :art: CSS SideBar / InputSearch

--- panda_store/src/App.js
+++ panda_store/src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 import Home from './Pages/Home';
 
+import pandaHeader from './images/pandaHeader.png';
+
 function App() {
   return (
     <div>
-      <Home />
+      <header className="header">
+        <img src={pandaHeader} alt="urso panda" width="65px" />
+        <h1>PANDA STORE</h1>
+      </header>
+      <div>
+        <Home />
+      </div>
     </div>
   );
 }
