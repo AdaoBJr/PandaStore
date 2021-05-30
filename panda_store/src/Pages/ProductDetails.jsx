63e0ead7caa1d83012e8b4ac2ee996c4557a6f35
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-05-29 21:37:20 -0400

    feat: :zap: pull main

--- panda_store/src/Pages/ProductDetails.jsx
+++ panda_store/src/Pages/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ProductDetails extends Component {
   constructor() {
@@ -7,10 +8,30 @@ export default class ProductDetails extends Component {
   }
 
   render() {
+    const { location: { state: { product } } } = this.props;
+    const {
+      title, thumbnail, price, attributes,
+    } = product;
     return (
       <div>
-        teste
+        <p>{title}</p>
+        <p>{thumbnail}</p>
+        <p>{price}</p>
+        <p>{attributes}</p>
       </div>
     );
   }
 }
+
+ProductDetails.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      product: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        thumbnail: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        attributes: PropTypes.arrayOf(PropTypes.object).isRequired,
+      }),
+    }),
+  }).isRequired,
+};
